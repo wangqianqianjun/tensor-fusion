9d00999dfc52e5871fada16e34bca11b22515392
Joey Yang <14833440+Code2Life@users.noreply.github.com>
2025-07-17 17:43:12 +0800

    fix: detect used by bug (#284)

--- internal/constants/env.go
+++ internal/constants/env.go
@@ -119,7 +119,7 @@ const (
 	HypervisorListenAddrEnv         = "API_LISTEN_ADDR"
 	HypervisorMetricsFormatEnv      = "TF_HYPERVISOR_METRICS_FORMAT"
 	HypervisorMetricsExtraLabelsEnv = "TF_HYPERVISOR_METRICS_EXTRA_LABELS"
-	HypervisorDetectUsedGPUEnv      = "DETECT_IN_USED_GPU"
+	HypervisorDetectUsedGPUEnv      = "DETECT_IN_USED_GPUS"
 
 	HypervisorDefaultPortNumber int32  = 8000
 	HypervisorPortName          string = "http"
